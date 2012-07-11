92a59f0a994cf7e7a8d6a3193052afde297c8050
Peng Wang <rocinwinter@gmail.com>
2012-07-11 22:08:15 +0800

    Add touch icon for iOS

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,17 +6,17 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php echo CHtml::encode(Yii::app()->name); ?>">
 	<meta name="author" content="rocwang">
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+
 	<link rel="stylesheet" href="css/style.css" media="screen" />
+	<link rel="icon" type="image/png" href="<?php echo Yii::app()->baseUrl; ?>/img/apple-touch-icon-114.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo Yii::app()->baseUrl; ?>/img/apple-touch-icon-114.png">
+	<!--<link rel="apple-touch-startup-image" href="/startup.png">-->
 
 	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-
-	<!--link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/ico/favicon.ico">
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Yii::app()->request->baseUrl; ?>/ico/apple-touch-icon-114-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Yii::app()->request->baseUrl; ?>/ico/apple-touch-icon-72-precomposed.png">
-	<link rel="apple-touch-icon-precomposed" href="<?php echo Yii::app()->request->baseUrl; ?>/ico/apple-touch-icon-57-precomposed.png"-->
 </head>
 <body>
 	<?php $this->widget('bootstrap.widgets.BootNavbar', array(

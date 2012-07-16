4abd5528b7809d495d08121b546f6491fd70b762
roc <rocinwinter@gmail.com>
2012-07-16 11:10:03 +0000

    Use localStorage to improve experience

--- www/js/incoming.js
+++ www/js/incoming.js
@@ -1,20 +1,35 @@
+$(function(){
+
 function refresh()
 {
 	$('img.loader').show();
 
 	$.getJSON('api.php', {r:'site/list'}, function(data) {
-		var output = Mustache.render($('#greader').html(), data);
-		$('#accordion').html(output);
+		//保存本次结果
+		localStorage.clear();
+		var items = data.items;
+		for(var i = 0; i < items.length; i++) {
+			localStorage[items[i].id] = JSON.stringify(items[i]);
+		}
 
-		//初始化hammer.js
-		$('.accordion-heading')
-		.hammer({
-			swipe_time         : 2000,
-			swipe_min_distance : 100
-		});
+		render(data);
 
 		//隐藏loader
 		$('img.loader').hide();
+	});
+}
+
+function render(data)
+{
+	var output = Mustache.render($('#greader').html(), data);
+	$('#accordion').html(output);
+
+
+	//初始化hammer.js
+	$('.accordion-heading')
+	.hammer({
+		swipe_time         : 2000,
+		swipe_min_distance : 100
 	});
 }
 
@@ -26,20 +41,34 @@ $('#accordion').on('swipe', '.accordion-heading', function(ev) {
 			}
 		);
 
+		var id = $(ev.target).attr('item_id');
+
 		$.getJSON(
 			'api.php',
 			{
 				r  : 'site/markRead',
-				id : $(ev.target).attr('item_id')
+				id : id
 			},
 			function(data, textStatus, jqXHR) {
 				//if (jqXHR.status === 200) {
 				//}
 			}
 		);
+
+		localStorage.removeItem(id);
 	}
 });
 
 $('#refresh').click(refresh);
 
-refresh();
+if (localStorage.length > 0) {
+	var items = [];
+	for(var i = 0; i < localStorage.length; i++) {
+		items[i] = JSON.parse(localStorage[localStorage.key(i)]);
+	}
+	render({items: items});
+} else {
+	refresh();
+}
+
+});
